dd1648809167e90ea557300eee8342e13b12b19f
3piX <marc.matthiesen@gmail.com>
2015-06-09 14:32:42 +0200

    :)

---
+++ WheeledRobotRemoteControlV1/APP/inc/ADC.h
@@ -0,0 +1,15 @@
+/*
+ * ADC.h
+ *
+ *  Created on: 09/06/2015
+ *      Author: Marc
+ */
+
+#ifndef APP_INC_ADC_H_
+#define APP_INC_ADC_H_
+
+
+
+
+
+#endif /* APP_INC_ADC_H_ */
---
+++ WheeledRobotRemoteControlV1/APP/src/ADC.c
@@ -0,0 +1,10 @@
+/*
+ * ADC.c
+ *
+ *  Created on: 09/06/2015
+ *      Author: Marc
+ */
+
+
+
+
